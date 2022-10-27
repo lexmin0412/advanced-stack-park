a97d218aec75bf1cea771d2852a76dd737a638c6
lexmin0412 <zhangle_dev@outlook.com>
2022-10-27 12:55:54 +0800

    chore: ci 流程新增站点截图

---
+++ scripts/capture-website.mjs
@@ -0,0 +1,14 @@
+import * as fs from 'fs'
+import * as path from 'path'
+import { fileURLToPath } from 'url'
+import captureWebsite from 'capture-website';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const screenshotPath = path.resolve(__dirname, '..', 'static', 'website.png')
+
+if (fs.existsSync(screenshotPath)) {
+	fs.rmSync(screenshotPath)
+}
+
+await captureWebsite.file('https://lexmin0412.github.io/advanced-stack-park', 'static/website.png');
